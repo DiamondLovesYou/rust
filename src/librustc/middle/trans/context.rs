a3c68035fafcc28b56f43f205f12e1c1b2d55282
Richard Diamond <wichard@vitalitystudios.com>
2014-06-30 23:41:00 -0500

    librustc: PNaCl doesn't support split stacks.

--- src/librustc/middle/trans/context.rs
+++ src/librustc/middle/trans/context.rs
@@ -283,7 +283,7 @@ impl CrateContext {
     // but it could be enabled (with patched LLVM)
     pub fn is_split_stack_supported(&self) -> bool {
         let ref cfg = self.sess().targ_cfg;
-        cfg.os != abi::OsiOS || cfg.arch != abi::Arm
+        (cfg.os != abi::OsiOS || cfg.arch != abi::Arm) && cfg.arch != abi::Le32
     }
 }
 
