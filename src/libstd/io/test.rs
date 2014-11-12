8ce7aa2f9cda7a9c950146f0a27e5fd2f6560612
Richard Diamond <wichard@vitalitystudios.com>
2014-11-12 11:54:37 -0600

    Fix a test helper.

--- src/libstd/io/test.rs
+++ src/libstd/io/test.rs
@@ -28,9 +28,9 @@ pub fn next_test_port() -> u16 {
 pub fn next_test_unix() -> Path {
     static COUNT: AtomicUint = INIT_ATOMIC_UINT;
 
-    #[cfg(all(not(target_os = "nacl"), not(target_libc = "newlib")))]
+    #[cfg(not(target_os = "nacl"))]
     fn getpid() -> libc::pid_t { unsafe { libc::getpid() } }
-    #[cfg(all(target_os = "nacl", target_libc = "newlib"))]
+    #[cfg(target_os = "nacl")]
     fn getpid() -> libc::pid_t {
         use rand::random;
         random()
