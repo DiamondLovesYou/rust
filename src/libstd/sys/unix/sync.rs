b22bd8d6a8c5866b251b38b72f34d33586354076
Richard Diamond <wichard@vitalitystudios.com>
2015-01-20 17:40:44 -0600

    pthread_cond_timedwait is #define-ed to pthread_cond_timedwait_abs on Newlib.

--- src/libstd/sys/unix/sync.rs
+++ src/libstd/sys/unix/sync.rs
@@ -26,6 +26,9 @@ extern {
     // cvars
     pub fn pthread_cond_wait(cond: *mut pthread_cond_t,
                              lock: *mut pthread_mutex_t) -> libc::c_int;
+
+    #[cfg_attr(target_libc = "newlib",
+               link_name = "pthread_cond_timedwait_abs")]
     pub fn pthread_cond_timedwait(cond: *mut pthread_cond_t,
                               lock: *mut pthread_mutex_t,
                               abstime: *const libc::timespec) -> libc::c_int;
