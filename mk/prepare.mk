f1762084cb4b18a03eeb8bdf120794ec4036dda4
Richard Diamond <wichard@vitalitystudios.com>
2015-04-14 00:22:30 -0500

    Clean up two lingering uses of DISABLED_CRATES.

--- mk/prepare.mk
+++ mk/prepare.mk
@@ -130,7 +130,7 @@ define DEF_PREPARE_TARGET_N
 prepare-target-$(2)-host-$(3)-$(1)-$(4): PREPARE_WORKING_SOURCE_LIB_DIR=$$(PREPARE_SOURCE_LIB_DIR)/rustlib/$(2)/lib
 prepare-target-$(2)-host-$(3)-$(1)-$(4): PREPARE_WORKING_DEST_LIB_DIR=$$(PREPARE_DEST_LIB_DIR)/rustlib/$(2)/lib
 prepare-target-$(2)-host-$(3)-$(1)-$(4): prepare-maybe-clean-$(4) \
-        $$(foreach crate, $$(filter-out $$(DISABLED_CRATES_$(2)), $$(TARGET_CRATES)), \
+        $$(foreach crate, $$(TARGET_CRATES), \
           $$(TLIB$(1)_T_$(2)_H_$(3))/stamp.$$(crate)) \
         $$(if $$(findstring $(2),$$(CFG_HOST)), \
           $$(foreach crate,$$(HOST_CRATES), \
@@ -143,7 +143,7 @@ prepare-target-$(2)-host-$(3)-$(1)-$(4): prepare-maybe-clean-$(4) \
       $$(if $$(findstring $(2), $$(PREPARE_TARGETS)),\
         $$(if $$(findstring $(3), $$(PREPARE_HOST)),\
           $$(call PREPARE_DIR,$$(PREPARE_WORKING_DEST_LIB_DIR))\
-          $$(foreach crate,$$(filter-out $$(DISABLED_CRATES_$(2)), $$(TARGET_CRATES)),\
+          $$(foreach crate,$$(TARGET_CRATES),\
             $$(if $$(SHARED_LIBS_DISABLED_$(2)),,\
               $$(if $$(or $$(findstring 1, $$(ONLY_RLIB_$$(crate))),$$(findstring 1,$$(CFG_INSTALL_ONLY_RLIB_$(2)))),, \
                 $$(call PREPARE_LIB,$$(call CFG_LIB_GLOB_$(2),$$(crate)))))\
